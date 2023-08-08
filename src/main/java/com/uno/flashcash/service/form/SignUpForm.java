c01c9634a0dff90772fdb84306c3a472cb2bd802
Bachir Nabil <bachirnabil83@gmail.com>
2023-08-08 09:15:01 +0200

    feature : adding registration template

--- src/main/java/com/uno/flashcash/service/form/SignUpForm.java
+++ src/main/java/com/uno/flashcash/service/form/SignUpForm.java
@@ -1,14 +1,31 @@
 package com.uno.flashcash.service.form;
 
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+
 
 @Data
 public class SignUpForm {
+
+    @NotBlank(message = "Le prénom ne peut pas être vide")
     private String firstName;
+
+    @NotBlank(message = "Le nom de famille ne peut pas être vide")
     private String lastName;
+
+    @NotBlank(message = "L'adresse e-mail ne peut pas être vide")
+    @Email(message = "L'adresse e-mail n'est pas valide")
     private String email;
+
+    @NotBlank(message = "Le mot de passe ne peut pas être vide")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères")
     private String password;
+
+    @NotBlank(message = "La confirmation du mot de passe ne peut pas être vide")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères")
     private String confirmPassword;
 }
 
